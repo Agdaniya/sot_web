72ac604c516df6bda2aa91755aac6bc93a082e66
Agdaniya <niyaagda@gmail.com>
2026-03-28 21:56:15 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sot_web.git/',  // ← replace with your actual repo name
+  base: '/sot_web/',
 })
